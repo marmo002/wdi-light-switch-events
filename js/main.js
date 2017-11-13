858df66d2d452a0404de83e9ff3012101d7c7789
marmo002 <marmo_02@hotmail.com>
2017-11-13 14:48:01 -0500

    finish assignment

--- js/main.js
+++ js/main.js
@@ -1,2 +1,28 @@
+document.addEventListener('DOMContentLoaded', function () {
 
+  var switchButton = document.querySelector('button');
+  var bodyElement = document.querySelector('body');
 
+  switchButton.addEventListener('click', function(){
+
+    var switchCurrentClass = switchButton.getAttribute('class');
+    var bodyClass = bodyElement.getAttribute('class');
+    var docH1 = document.querySelector('h1');
+
+    if (switchCurrentClass === 'switch on'){
+      switchButton.setAttribute('class', 'switch off');
+      docH1.innerText = "Hey, who turned off the lights?";
+    } else {
+      switchButton.setAttribute('class', 'switch on');
+      docH1.innerText = "It's so bright in here!";
+    }
+
+    if (bodyClass === 'light'){
+      bodyElement.setAttribute('class', 'dark');
+    } else {
+      bodyElement.setAttribute('class', 'light');
+    }
+
+  });
+
+});
